7b90be913255cc105d78dcb0e329484c9e60ae36
Inna Cervenkova <inna.cervenkova@gmail.com>
2023-04-02 14:37:33 +0200

    minor adjustments

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 console.log('funguju!');
+
+const questionElm = document.querySelector('.question');
+const question = "Uveďte příjmení amerického vynálezce, který v roce 1876 jako první úspěšně podal patent na zařízení, které je dnes považováno za první telefon.";
+questionElm.textContent = question;
+
+
+const userAnswear = prompt(question);
+
+const answearTextElm = document.querySelector('.answear__text');
+answearTextElm.textContent = userAnswear;
+
+const answearElm = document.querySelector('.answear');
+
+if (answear === 'Bell') {
+  answearElm.classList.add('answer--correct');
+} else {
+  answearElm.classList.add('answer--wrong');
+}
