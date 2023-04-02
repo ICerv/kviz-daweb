7893ba17f2882e61536121f0059757710297800c
Inna Cervenkova <inna.cervenkova@gmail.com>
2023-04-02 15:13:03 +0200

    The code is not functioning as intended.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ console.log('funguju!');
 
 const questionElm = document.querySelector('.question');
 const question = "Uveďte příjmení amerického vynálezce, který v roce 1876 jako první úspěšně podal patent na zařízení, které je dnes považováno za první telefon.";
+
 questionElm.textContent = question;
 
 
@@ -12,8 +13,8 @@ answearTextElm.textContent = userAnswear;
 
 const answearElm = document.querySelector('.answear');
 
-if (answear === 'Bell') {
-  answearElm.classList.add('answer--correct');
+if (userAnswear === 'Bell') {
+  answearElm.classList.add('answear--correct');
 } else {
-  answearElm.classList.add('answer--wrong');
+  answearElm.classList.add('answear--wrong');
 }
